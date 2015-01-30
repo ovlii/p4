224a036bea7654ef1592287e992abb53527a8a96
Venkat Annangi <vannangi@g.harvard.edu>
2015-01-30 01:31:28 -0600

    Fixed layouts

--- app/views/listing_index.blade.php
+++ app/views/listing_index.blade.php
@@ -23,16 +23,16 @@
                         <h4><i class="fa fa-fw fa-check"></i> {{$listing['short_title']}} </h4>
                     </div>
                     <div class="panel-body">
-                        <p>{{$listing['description']}}</p>
+                        <p><b>Description :</b> {{$listing['description']}}</p>
                         
-                        <p>{{$listing['full_name']}}</p>
+                        <p><b>Contact Name : </b>{{$listing['full_name']}}</p>
 
-                        <p>{{$listing['email_address']}}</p>
+                        <p><b>Contact Email :</b> {{$listing['email_address']}}</p>
 
-                        <p>{{$listing['phone_number']}}</p>
+                        <p><b>Contact Phone :</b> {{$listing['phone_number']}}</p>
                         
-                        <p>{{$listing['price']}}</p>
-                        <p>
+                        <p><b>Price : </b>{{$listing['price']}}</p>
+                        <p> <b>Categories: </b>
                             @foreach($listing['categories'] as $category)
                                 <span class='category'>{{{ $category->name }}}</span>
                             @endforeach
